2ca7c894a3d432c12484ffaf5f3e57a14077c393
Ashmit Handoo <ashmithandoo@outlook.com>
2026-05-20 15:40:46 -0400

    fixed wsl_train_Iter&Itra_Class.py

--- weak_supervised_segmentation/wsl_train_Inter&Intra_Class.py
+++ weak_supervised_segmentation/wsl_train_Inter&Intra_Class.py
@@ -146,7 +146,7 @@ class Trainer:
             self.batch_time.update(time.time() - tic)
 
             self._metrics_update(
-                *get_metrics(torch.softmax(pre[:, :self.config.DATASET.NUM_CLASSES], dim=1).cpu().detach().numpy()[:, 1, :, :], gt.cpu().detach().numpy()).values())
+                **get_metrics(torch.softmax(pre[:, :self.config.DATASET.NUM_CLASSES], dim=1).cpu().detach().numpy()[:, 1, :, :], gt.cpu().detach().numpy()))
             tbar.set_description(
                 'TRAIN ({}) | Loss: {:.4f} | AUC {:.4f} F1 {:.4f} Acc {:.4f}  Sen {:.4f} Spe {:.4f} Pre {:.4f} IOU {:.4f} |B {:.2f} D {:.2f} |'.format(
                     epoch, self.total_loss.mean, *self._metrics_ave().values(), self.batch_time.mean, self.data_time.mean))
@@ -179,7 +179,7 @@ class Trainer:
 
                 self.total_loss.update(loss.item())
                 self._metrics_update(
-                    *get_metrics(torch.softmax(predict[:, :self.config.DATASET.NUM_CLASSES], dim=1).cpu().detach().numpy()[:, 1, :, :], gt.cpu().detach().numpy()).values())
+                    **get_metrics(torch.softmax(predict[:, :self.config.DATASET.NUM_CLASSES], dim=1).cpu().detach().numpy()[:, 1, :, :], gt.cpu().detach().numpy()))
                 tbar.set_description(
                     'EVAL ({})  | Loss: {:.4f} | AUC {:.4f} F1 {:.4f} Acc {:.4f} Sen {:.4f} Spe {:.4f} Pre {:.4f} IOU {:.4f} |'.format(
                         epoch, self.total_loss.mean, *self._metrics_ave().values()))
