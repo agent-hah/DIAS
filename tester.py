4ca18c9fe17ed24380f6fe38ce335d367bde0411
Ashmit Handoo <ashmithandoo@outlook.com>
2026-05-21 16:25:18 -0400

    fix: update string formatting in tester.py for LaTeX compatibility

--- tester.py
+++ tester.py
@@ -116,7 +116,7 @@ class Tester(Trainer):
         columns.append("VC")
 
 
-        formatted_data = [f"{mean}$\pm${std}" for mean, std in zip(mean_data, std_data)]
+        formatted_data = [rf"{mean}$\pm${std}" for mean, std in zip(mean_data, std_data)]
 
         # 创建一个字典，用于构造DataFrame
         data_dict = {col: [val] for col, val in zip(columns, formatted_data)}
