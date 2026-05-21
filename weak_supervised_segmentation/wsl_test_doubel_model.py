5c076bde65255279fefc236b200d59d966cd7019
Ashmit Handoo <ashmithandoo@outlook.com>
2026-05-21 16:45:15 -0400

    fix: ensure color output is in uint8 format and correct CSV file extension in wsl_test_DMPLS.py and wsl_test_doubel_model.py

--- weak_supervised_segmentation/wsl_test_doubel_model.py
+++ weak_supervised_segmentation/wsl_test_doubel_model.py
@@ -93,7 +93,7 @@ class Tester(Trainer):
             cv2.imwrite(os.path.join(self.save_path, f"gt{j}.png"), (np.squeeze(gts[j])*255).astype(np.uint8))
             cv2.imwrite(os.path.join(self.save_path, f"pre{j}.png"), (np.squeeze(predict[j])*255).astype(np.uint8))
             cv2.imwrite(os.path.join(self.save_path, f"pre_b{j}.png"), (np.squeeze(predict_b[j])*255).astype(np.uint8))
-            cv2.imwrite(os.path.join(self.save_path, f"color_b{j}.png"), get_color(predict_b[j], gts[j]))
+            cv2.imwrite(self.save_path + f"/color_b{j}.png", get_color(predict_b[j],gts[j]).astype(np.uint8))
             
             self._update_metrics(*get_metrics(predict[j], gts[j],run_clDice= True).values())
             self.VC.update(count_connect_component(predict_b[j], gts[j]))
@@ -110,19 +110,19 @@ class Tester(Trainer):
         columns.append("VC")
 
 
-        formatted_data = [f"{mean}$\pm${std}" for mean, std in zip(mean_data, std_data)] # type: ignore
+        formatted_data = [rf"{mean}$\pm${std}" for mean, std in zip(mean_data, std_data)] # type: ignore
 
         # 创建一个字典，用于构造DataFrame
         data_dict = {col: [val] for col, val in zip(columns, formatted_data)}
 
         # 创建DataFrame
         df = pd.DataFrame(data_dict)
-        df.to_csv(join(self.save_path, f"{self.model_name}_result.cvs"))
+        df.to_csv(join(self.save_path, f"{self.model_name}_result.csv"))
         for k, v in self._get_metrics_mean().items():
-            logger.info(f'{str(k):5s}: {v}')
+            logger.info(f'{str(k):5s} (Mean) : {v}')
 
         for k, v in self._get_metrics_std().items():
-            logger.info(f'{str(k):5s}: {v}')
+            logger.info(f'{str(k):5s} (Std)  : {v}')
         
         logger.info(f'VC_mean: {self.VC.mean}')
        
